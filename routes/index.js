1c2ac1d951471ac3fff5fdf9347e42f8dc566f27
nurhandiyudi <nurhandiy@gmail.com>
2018-04-13 09:36:12 +0000

    update link

--- routes/index.js
+++ routes/index.js
@@ -137,20 +137,29 @@ router.get('/subscribe-done', function(req, res, next) {
 
 
 /* Promo Flyer. */
+//Promo Location 1 http://bit.ly/2qti6O6
 router.get('/864c445f0bd5c26cb3dbbaa08a82c850daaaa70f6a362cb1f0fd72be18810a51', function(req, res, next) {
-  res.render('index', { title: 'Lokasi1' });
+  res.render('index', { title: 'Loc.1' });
 });
+
+//Promo Location 2 http://bit.ly/2qvjW1K
 router.get('/388832377c81c5e8a4043ed83fc580f405c07c4eaed4e07e04e81905f2f5a5f9', function(req, res, next) {
-  res.render('index', { title: 'Lokasi2' });
+  res.render('index', { title: 'Loc.2' });
 });
+
+//Promo Location 3 http://bit.ly/2GUXCZB
 router.get('/b68f9b1194550f7b4f4499cab6005c45551cf46d7a8b544f77d1c8b94d440c32', function(req, res, next) {
-  res.render('index', { title: 'Lokasi3' });
+  res.render('index', { title: 'Loc.3' });
 });
+
+//Promo Location 4 http://bit.ly/2qt8UKD
 router.get('/9085f97251027575d6fe5f69ed34ce2ef0c5634d539f03255a78a67d52525a43', function(req, res, next) {
-  res.render('index', { title: 'Lokasi4' });
+  res.render('index', { title: 'Loc.4' });
 });
+
+//Promo Location 5 http://bit.ly/2vgmQw2
 router.get('/ab86e4f114cb4043cac70b3f2af12f61434962087c477da92c8132ffad43cf51', function(req, res, next) {
-  res.render('index', { title: 'Lokasi5' });
+  res.render('index', { title: 'Loc.5' });
 });
 
 // Add Submit POST Route
